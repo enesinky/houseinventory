08874dc16567ca5f6a918cb7555f6da736785dc0
Enes Inkaya <enes.inkaya1@huawei.com>
2020-09-23 12:02:57 +0300

    refactored imports

--- lib/ui/cards/location_card.dart
+++ lib/ui/cards/location_card.dart
@@ -1,4 +1,4 @@
-import 'package:houseinventory/pages/inventory/inventory_location.dart';
+import 'package:houseinventory/pages/inventory/item_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 

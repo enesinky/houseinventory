6b06b0b6d50857e47b28694efbc77ec628be9f8c
Enes Inkaya <enes.inkaya1@huawei.com>
2020-09-24 17:58:02 +0300

    builder parameter changed to List

--- lib/util/path.dart
+++ lib/util/path.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 class Path {
 
   final String pattern;
-  final Widget Function(BuildContext, String) builder;
+  final Widget Function(BuildContext, List) builder;
 
   const Path(this.pattern, this.builder);
 }

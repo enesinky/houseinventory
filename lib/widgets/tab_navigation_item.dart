62fdea4fba3f6bfc385e0e3e892786ddf6d49e50
Enes Inkaya <enes.inkaya1@huawei.com>
2020-09-24 17:59:01 +0300

    imports changed.

--- lib/widgets/tab_navigation_item.dart
+++ lib/widgets/tab_navigation_item.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import '../pages/search/search.dart';
 import '../pages/dashboard/dashboard.dart';
 import '../pages/settings/settings.dart';
-import '../pages/inventory/location_view.dart';
+import '../pages/inventory/inventory_view.dart';
 
 class TabNavigationItem {
   final Widget page;
@@ -24,7 +24,7 @@ class TabNavigationItem {
       title: Text("Dashboard"),
     ),
     TabNavigationItem(
-      page: LocationViewPage(),
+      page: InventoryViewPage(),
       icon: Icon(Icons.view_list),
       title: Text("Inventory List"),
     ),

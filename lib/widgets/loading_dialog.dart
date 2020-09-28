538b54b8543f6db3bbef75c649c49c2a34a6496d
Enes Inkaya <enes.inkaya1@huawei.com>
2020-09-28 14:36:10 +0300

    added custom widget.

---
+++ lib/widgets/loading_dialog.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomLoadingDialog {
+
+  static var _widget = Dialog(
+    child: Center(
+      heightFactor: 1.6,
+      widthFactor: 1,
+      child: new Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          new CircularProgressIndicator(),
+          SizedBox(height: 24,),
+          new Text("Updating Inventory", style: TextStyle(
+              color: Colors.black54,
+              fontSize: 15
+          ),),
+        ],
+      ),
+    ),
+  );
+
+  static get widget => _widget;
+}

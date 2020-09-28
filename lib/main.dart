23fc9f1e791259f943e8a57a6b9d7ac427f1d150
Enes Inkaya <enes.inkaya1@huawei.com>
2020-09-28 21:42:40 +0300

    import new file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:houseinventory/pages/start/start.dart';
 import 'package:houseinventory/util/route_configuration.dart';
 
 import 'data/process_data.dart';
@@ -17,12 +18,11 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
 
+  var user;
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
-
-
     return MaterialApp(
       title: 'House Inventory',
       theme: ThemeData(
@@ -32,22 +32,8 @@ class MyApp extends StatelessWidget {
       ),
       //home: TabsPage(),
       debugShowCheckedModeBanner: false,
-      initialRoute: TabsPage.route,
+      initialRoute: user == null ? StartPage.route : TabsPage.route,
       onGenerateRoute: RouteConfiguration.onGenerateRoute,
-      // Start the app with the "/" named route. In this case, the app starts
-      // on the FirstScreen widget.
-      //initialRoute: '/',
-      // routes: {
-      //   // When navigating to the "/" route, build the FirstScreen widget.
-      //   '/': (context) => TabsPage(),
-      //   // When navigating to the "/second" route, build the SecondScreen widget.
-      //   InventoryPage.route: (context) => InventoryPage(),
-      //   Dashboard.route: (context) => Dashboard(),
-      //   SettingsPage.route: (context) => SettingsPage(),
-      //   SearchPage.route: (context) => SearchPage()
-      //
-      // },
-
     );
   }
 }
